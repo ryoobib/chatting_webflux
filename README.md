e6bbf0b30f2417d4188ebb5e7945b6fafc0ed914
dldmsql <yhy5913@naver.com>
2022-08-23 10:32:09 +0900

    chore: basic setting for project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # chatting_webflux
+
 This repository is chatting service with webflux
---
+++ src/main/java/com/ryoobib/chatting_webflux/ChattingWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.ryoobib.chatting_webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChattingWebfluxApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ChattingWebfluxApplication.class, args);
+  }
+
+}
